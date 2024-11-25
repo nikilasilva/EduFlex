699bb4497709a3832b70d572f2dd70b82d9e6d37
CharithaGamlatha <gamlathcharitha@gmail.com>
2024-11-25 15:23:23 +0530

    update nav bar icans

--- app/views/inc/nonAcademic/sidebar_nonAcademic.php
+++ app/views/inc/nonAcademic/sidebar_nonAcademic.php
@@ -16,11 +16,11 @@
     <div class="menu">
         <ul class="menu-links">
             <li class="nav-links"><a href="<?php echo URLROOT ?>/Dashboard/index"><i class="fa-solid fa-house icon"></i><span class="text nav-text">Home</span></a></li>
-            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/Issuance_books"><i class="fa-solid fa-book icon" aria-hidden="true"></i><span class="text nav-text">Issuance Books</span></a></li>
-            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/ReceipfBooks"><i class="fa-solid fa-house icon"></i><span class="text nav-text">Issuance Books</span></a></li>
-            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/viewActivities"><i class="fa-sharp-duotone fa-light fa-book-copy"></i><span class="text nav-text">See Library Activity</span></a></li>
-            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/TeachersAttendencee"><i class="fa-solid fa-house icon"></i><span class="text nav-text">Teachers Attendencee</span></a></li>
-            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/checkServiceCharges"><i class="fa-solid fa-house icon"></i><span class="text nav-text">Service Charges</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/Issuance_books"><i class="fa-solid fa-book icon" aria-hidden="true"></i></i><span class="text nav-text">Issuance Books</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/ReceipfBooks"><i class="fa-solid fa-book icon"></i><span class="text nav-text">Issuance Books</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/viewActivities"><i class="fa-solid fa-book icon"></i><span class="text nav-text">See Library Activity</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/TeachersAttendencee"><i class="fa-solid fa-users icon"></i><span class="text nav-text">Teachers Attendencee</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/checkServiceCharges"><i class="fa-solid fa-usd icon"></i><span class="text nav-text">Service Charges</span></a></li>
             <li class="nav-links"><a href="#misReport"><i class="fa-solid fa-file icon"></i><span class="text nav-text">MIS Report</span></a></li>
             <li class="nav-links"><a href="<?php echo URLROOT ?>/Teacher/dailyActivities"><i class="fa-solid fa-business-time icon"></i><span class="text nav-text">Teacher's Record</span></a></li>
             <li class="nav-links"><a href="<?php echo URLROOT ?>/Feedback/index"><i class="fa-solid fa-comment icon"></i><span class="text nav-text">Feedbacks</span></a></li>
