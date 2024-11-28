eb79012e6b880a1c1017c72940da58c398a0a7ea
CharithaGamlatha <gamlathcharitha@gmail.com>
2024-11-28 11:35:45 +0530

    hh

--- app/views/inc/nonAcademic/sidebar_nonAcademic.php
+++ app/views/inc/nonAcademic/sidebar_nonAcademic.php
@@ -1,11 +1,30 @@
-<nav>
+z<!-- <nav>
     <ul>
-        <!-- Test data -->
         <li><a href="#home">Home</a></li>
         <li><a href="#students">Students</a></li>
         <li><a href="#teachers">Teachers</a></li>
         <li><a href="#timetable">Timetable</a></li>
         <li><a href="#scheduleEvents">Scheduled Events</a></li>
+        <li><a href="#misReport">MIS Report</a></li>
+        <li><a href="#currentActivities">Current Activities</a></li>
+        <li><a href="#feedback">Feedback</a></li>
         <li><a href="#settings">Settings</a></li>
     </ul>
-</nav>
+</nav> -->
+
+<div class="menu-bar">
+    <div class="menu">
+        <ul class="menu-links">
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/Dashboard/index"><i class="fa-solid fa-house icon"></i><span class="text nav-text">Home</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/Issuance_books"><i class="fa-solid fa-book icon" aria-hidden="true"></i></i><span class="text nav-text">Issuance Books</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/ReceipfBooks"><i class="fa-solid fa-book icon"></i><span class="text nav-text">Receipt Books</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/viewActivities"><i class="fa-solid fa-book icon"></i><span class="text nav-text">See Library Activity</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/TeachersAttendencee"><i class="fa-solid fa-users icon"></i><span class="text nav-text">Teachers Attendencee</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/NonAcademic/checkServiceCharges"><i class="fa-solid fa-usd icon"></i><span class="text nav-text">Service Charges</span></a></li>
+            <li class="nav-links"><a href="#misReport"><i class="fa-solid fa-file icon"></i><span class="text nav-text">MIS Report</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/Teacher/dailyActivities"><i class="fa-solid fa-business-time icon"></i><span class="text nav-text">Teacher's Record</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/Feedback/index"><i class="fa-solid fa-comment icon"></i><span class="text nav-text">Feedbacks</span></a></li>
+
+        </ul>
+    </div>
+</div>
