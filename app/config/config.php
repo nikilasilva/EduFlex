a0df77598a94b895f6ac0609d9d71191c9af20d9
Nikila Silva <118242917+nikilasilva@users.noreply.github.com>
2024-11-28 14:00:25 +0530

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'eduflex');
+    define('DB_NAME', 'eduflex_db');
 
     // APPROOT
     define('APPROOT', dirname(dirname(__FILE__)));
