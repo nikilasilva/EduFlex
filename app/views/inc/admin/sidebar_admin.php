670770ba180c0f203de95bec4ceb2b12ba3e2f3c
sadish1997 <sadishrandika@gmail.com>
2024-12-18 19:09:03 +0530

    2ndcom

--- app/views/inc/admin/sidebar_admin.php
+++ app/views/inc/admin/sidebar_admin.php
@@ -11,5 +11,43 @@
 </nav> -->
 
 
+<!--<nav>
+    <ul>
+        <li><a href="#students">Students</a></li>
+        <li><a href="#teachers">Teachers</a></li>
+        <li><a href="#principal">Principal</a></li>
+        <li><a href="#vicePrincipal">Vice Principal</a></li>
+        <li><a href="#nonAcademic">Non-Academic</a></li>
+        <li><a href="#addTimetable">Add Timetable</a></li>
+        <li><a href="#addClass">Add Class</a></li>
+    </ul>
+</nav>
+--> 
+<div class="menu-bar">
+    <div class="menu">
+        <ul class="menu-links">
+            <li class="nav-links"><a href="<?php echo URLROOT ?>"><i class="fa-solid fa-people-line icon"></i><span class="text nav-text">Manage Accounts</span></a></li>
 
+        
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/managestudent"><i class="fa-solid fa-people-line icon"></i><span class="text nav-text">Students</span></a></li>
+
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/Issuance_books"><i class="fa-solid fa-book icon" aria-hidden="true"></i></i><span class="text nav-text">Issuance Books</span></a></li>
+
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/manageparent"><i class="fa-solid fa-hands-holding-child icon"></i><span class="text nav-text">Parents</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/manageteacher"><i class="fa-solid fa-person-chalkboard icon"></i><span class="text nav-text">Teachers</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/manageprincipal"><i class="fa-solid fa-person-half-dress icon"></i><span class="text nav-text">Principal</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/manageviceprincipal"><i class="fa-solid fa-person-hiking icon"></i><span class="text nav-text">Vice-Principal</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/manage_nonaca"><i class="fa-solid fa-person-shelter icon"></i><span class="text nav-text">Non-Academic</span></a></li>
+
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/ manage_MIS"><i class="fa-solid fa-id-card icon"></i><span class="text nav-text">MIS-Report</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/inser_tCLR"><i class="fa-solid fa-house icon"></i><span class="text nav-text">Add-Class-Room</span></a></li>
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/manage_class_timetable"><i class="fa-regular fa-calendar-days icon"></i><span class="text nav-text">Manage-Class-Time-Table</span></a></li>
+
+            
+
+            <li class="nav-links"><a href="<?php echo URLROOT ?>/admin/insert_aca_time_table"><i class="fa-regular fa-calendar-days icon"></i><span class="text nav-text">Manage Other Activity</span></a></li>
+            
+        </ul>
+    </div>
+</div> 
 
