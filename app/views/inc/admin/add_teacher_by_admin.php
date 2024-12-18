c1dc0d8ab5290868638c455aad126afc6fe5fd98
sadish1997 <sadishrandika@gmail.com>
2024-12-19 00:09:50 +0530

    cm

--- app/views/inc/admin/add_teacher_by_admin.php
+++ app/views/inc/admin/add_teacher_by_admin.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Insert Teacher</title>
     <!-- Link to the CSS file -->
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/daily_activities.css.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/insert_actor_style.css">
 </head>
 <body>
 <div class="admin_layout">
