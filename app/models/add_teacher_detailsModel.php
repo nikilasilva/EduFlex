b76f496362d4e1157b8ebd471513b13cebe22376
sadish1997 <sadishrandika@gmail.com>
2024-12-19 00:51:47 +0530

    cm3

--- app/models/add_teacher_detailsModel.php
+++ app/models/add_teacher_detailsModel.php
@@ -1,4 +1,22 @@
-<?php
+<?php 
+class add_teacher_detailsModel{
 
+    use Model;
 
-?>
+    protected $table = 'teacher_details';
+    protected $allowedColumns = [
+        "regNo",
+        "email",
+        "mobileNo",
+        "address",
+        "username",
+        "password",
+        "dob",
+        "gender",
+        "religion",
+        "role"
+    ];
+
+    protected $order_column = 'regNo'; // Defined here
+}
+
