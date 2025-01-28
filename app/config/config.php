c03f29db526190e8486e1fde6c20d64932f363fe
CharithaGamlatha <gamlathcharitha@gmail.com>
2025-01-28 17:24:52 +0530

    db name

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,15 @@
 <?php
-    //Database configuration
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'eduflex');
+//Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'eduflex_db');
 
-    // APPROOT
-    define('APPROOT', dirname(dirname(__FILE__)));
+// APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
 
-    // URLROOT
-    define('URLROOT', 'http://localhost/EduFlex');
+// URLROOT
+define('URLROOT', 'http://localhost/EduFlex');
 
-    // Website name
-    define('SITENAME', 'EduFlex');
-?>
+// Website name
+define('SITENAME', 'EduFlex');
