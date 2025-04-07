0ac789d5c6aea08caa0cb7ff837c2ac31a6412d9
nikilasilva <nikilasilva@gmail.com>
2025-04-07 16:14:16 +0530

    complete current activities functionality

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'eduflex');
+    define('DB_NAME', 'eduflex2');
     // define('DB_HOST', "mysql-nikila.alwaysdata.net");
     // define('DB_USER', 'nikila');
     // define('DB_PASSWORD','Silva7890');
