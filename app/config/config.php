c115c1460e80a6d590c4471cf76a3697c125640d
nikilasilva <nikilasilva@gmail.com>
2025-04-07 16:35:10 +0530

    edit config file (add previous database name)

--- app/config/config.php
+++ app/config/config.php
@@ -3,11 +3,8 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'eduflex2');
-    // define('DB_HOST', "mysql-nikila.alwaysdata.net");
-    // define('DB_USER', 'nikila');
-    // define('DB_PASSWORD','Silva7890');
-    // define('DB_NAME', 'nikila_db');
+    define('DB_NAME', 'eduflex');
+    // define('DB_NAME', 'eduflex2');
 
     // APPROOT
     define('APPROOT', dirname(dirname(__FILE__)));
