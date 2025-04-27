cd6d8a935926c7197e057709d5c48f59f46b4582
PramodiPeshila <ppeshila7@gmail.com>
2025-04-27 23:43:17 +0530

    Added view and update leaving certificate files

---
+++ app/views/inc/student/EditLeavingCertificate.php
@@ -0,0 +1,24 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<?php require APPROOT . '/views/inc/components/topNavbar.php'; ?>
+<?php require APPROOT . '/views/inc/components/sideBar.php'; ?>
+
+
+<form action="" method="POST">
+    <input type="text" name='reason'>
+    <button type="submit">SUBMIT</button>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
